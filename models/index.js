0dfe5b01005f2028ed054734cebd546c0994b3e0
= <=>
2022-01-02 14:34:28 -0600

    created associations

---
+++ models/index.js
@@ -0,0 +1,40 @@
+//import models
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+
+//creates associations between models
+User.hasMany(Post, {
+    //connects to post through the user_id that is connected to the User(id)
+    foreignKey: 'user_id'
+});
+
+//reverse association by adding the following statement 
+//post can belong to one user, not many users
+Post.belongsTo(User, {
+    //using same foreign key
+    foreignKey: 'user_id',
+    onDelete:'SET NULL'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+//exports the Model as an object with Model as a property
+module.exports = { User, Post, Comment };
