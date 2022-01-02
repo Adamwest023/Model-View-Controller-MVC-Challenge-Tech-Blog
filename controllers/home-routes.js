3c92785e4566019dadb130b78a9d7f1eecb44949
= <=>
2022-01-02 14:22:29 -0600

    updated user model

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
 
 const sequelize = require('sequelize');
+
+const {User,Post,Comment} = require("../models");
+
+router.get('/', (res,req) => {
+    console.log();
+})
